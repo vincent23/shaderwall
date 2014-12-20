0f7ceb27ca2688eae3b2bdb4715285679f81560d
Vincent Schüßler <v.schuessler@gmail.com>
2014-12-20 10:49:23 +0100

    Add glsl mode and remove all others

--- static/shaderwall.js
+++ static/shaderwall.js
@@ -1,7 +1,7 @@
 var Shaderwall = function() {
 	this.editor = CodeMirror(document.body, {
 		value: "#ifdef GL_ES\nprecision mediump float;\n#endif\nvoid main(void)\n{\n\tgl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);\n}\n",
-		mode: "clike",
+		mode: "glsl",
 		lineNumbers: true,
 		indentWithTabs: true,
 		indentUnit: 4,
